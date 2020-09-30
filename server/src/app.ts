95bc78333a021d85bdcc69e52f313ef54a1defe9
Neil Clayton <neilclayton83@outlook.com>
2020-09-30 10:26:57 +0100

    Server and Client
    Code for server and client

---
+++ server/src/app.ts
@@ -0,0 +1,27 @@
+import express, { Express } from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import todoRoutes from './routes'
+
+const app: Express = express()
+
+const PORT: string | number = process.env.PORT || 4000
+
+app.use(cors())
+app.use(todoRoutes)
+
+// @ts-ignore
+const uri: string = process.env.MONGO_DB
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.set('useFindAndModify', false)
+
+mongoose
+    .connect(uri, options)
+    .then(() =>
+        app.listen(PORT, () =>
+            console.log(`Server running on http://localhost:${PORT}`)
+        )
+    )
+    .catch((error) => {
+        throw error
+    })
